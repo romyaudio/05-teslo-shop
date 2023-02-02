3897f7a48960661a78e17f1bd90f86b76cc6495c
Romy <romyaudio@hotmail.com>
2023-02-02 15:20:51 -0500

    continuacion-w

---
+++ pages/product/slug.tsx
@@ -0,0 +1,56 @@
+import { Box, Button, Chip, Grid, Typography } from '@mui/material'
+import React from 'react'
+import { ShopLayout } from '../../components/layouts'
+import { ProductSlideShow } from '../../components/products'
+import { initialData } from '../../database/products'
+
+const product = initialData.products[0]
+
+const ProductPage = () => (
+  <ShopLayout title={product.title} pageDescription={product.description}>
+    <Grid container spacing={3}>
+      
+      <Grid item xs={12} sm={7}>
+        {/* slideshow */}
+        <ProductSlideShow images={product.images}/>
+      </Grid>
+
+      <Grid item xs={12} sm={5}>
+        <Box display='flex' flexDirection='column'>
+
+          {/* titles */}
+          <Typography variant='h1' component='h1'>{product.title}</Typography>
+          <Typography variant='subtitle1' component='h2'>${product.price}</Typography>
+
+
+          {/* cantidad */}
+          <Box sx={{ my: 2 }}>
+            <Typography variant='subtitle2'>Cantidad</Typography>
+            {/* itemCounter */}
+          </Box>
+
+          {/* Agregar al carrito */}
+
+          <Button color='secondary' className='circular-btn'>
+            Agregar al carrito
+          </Button>
+
+          {/* <Chip label='No hay disponibles' color='error' variant='outlined'/> */}
+
+          {/* description */}
+
+          <Box sx={{mt:3}}>
+            <Typography variant='subtitle2'>Description</Typography>
+            <Typography variant='body2'>{product.description}</Typography>
+          </Box>
+
+        </Box>
+
+      </Grid>
+
+    </Grid>
+
+  </ShopLayout>
+)
+
+export default ProductPage
