5a4c94ff7a1215a87943343d336b008d7bf15c6c
Romy <romyaudio@hotmail.com>
2023-02-02 15:21:32 -0500

    continuacion-w

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,5 +1,7 @@
-import { Card, CardActionArea, CardMedia, Grid } from '@mui/material'
-import React, { FC } from 'react'
+import React, { FC, useMemo, useState } from 'react'
+import NextLink from 'next/link'
+import { Box, Card, CardActionArea, CardMedia, Grid, Link, Typography } from '@mui/material';
+
 import { IProduct } from '../../interfaces'
 
 interface Props {
@@ -7,19 +9,45 @@ interface Props {
 }
 
 export const ProductCard:FC<Props> = ({product}) => {
-  return (
-    <Grid item xs={6} sm={4} key={product.slug}>
-              <Card>
-                <CardActionArea>
-                  <CardMedia
-                  component={'img'}
-                  image={`products/${product.images[0]}`}
-                  alt={product.title}
-                  />
+  
+  const [isHovered, setIsHovered] = useState(false)
 
-                </CardActionArea>
+  const productImage = useMemo(() => {
+     return isHovered
+     ? `products/${product.images[1]}`
+     : `products/${product.images[0]}`
+  },
+
+   [isHovered,product.images])
+
+  return (
+    <Grid item xs={6} sm={4}
+     onMouseEnter={()=>setIsHovered(true)}
+     onMouseLeave={()=>setIsHovered(false)}
+     >
+              <Card>
+                <NextLink href='/product/slug' passHref legacyBehavior prefetch={false}>
+                  <Link>
+                   <CardActionArea>
+                    <CardMedia
+                     component={'img'}
+                     image={productImage}
+                     className='fadeIn'
+                     alt={product.title}
+                    />
+
+                   </CardActionArea>
+                  </Link>
+                </NextLink>
+                
 
               </Card>
+
+              <Box sx={{mt:1}} className='fadeIn'>
+                <Typography fontWeight={700}>{product.title}</Typography>
+                <Typography fontWeight={500}>${product.price}</Typography>
+
+              </Box>
 
             </Grid>
   )
