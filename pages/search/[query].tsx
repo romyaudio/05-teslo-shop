3a14106018b88119f6b4d8cd8d3f7551198cc737
romy <romyaudio@hotmail.com>
2023-02-08 22:16:33 -0500

    continuacio-h

--- pages/search/[query].tsx
+++ pages/search/[query].tsx
@@ -7,6 +7,7 @@ import { useProducts } from '../../hooks'
 
 const SearchPage = () => {
     const {products,isLoading,isError} = useProducts('/products')
+    
     return (
         <ShopLayout title="Teslo-Shop - Search" pageDescription={"Encuetra los mejores productos de teslo shop aqui"}>
            <Typography variant="h1" component='h1'>Buscar productos</Typography>
