763b80525f3414d51b5da21db9c1b186f01aa674
romy <romyaudio@hotmail.com>
2023-02-17 23:14:25 -0500

    continuacion-h

---
+++ pages/api/user/login.ts
@@ -0,0 +1,59 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { db } from '../../../database'
+import { User } from '../../../models'
+import bcrypt from 'bcryptjs'
+type Data = 
+|{message: string}
+|{
+    token:string
+    user:{
+        email:string
+        role:string
+        name:string
+    }
+}
+
+export default function (req: NextApiRequest, res: NextApiResponse<Data>) {
+    switch (req.method) {
+        case 'POST':
+            return loginUser(req,res)
+            
+          
+    
+        default:
+           res.status(400).json({
+            message:"Bad request"
+        })
+    }
+}
+
+const  loginUser = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+    const {email = '',password =''} = req.body
+
+    await db.connect()
+   const user = await User.findOne({email})
+   await db.disconnect()
+
+   if(!user){
+     return res.status(400).json({
+        message: 'Correo o contrasena no valido - EMAIL'
+     })
+   }
+
+   if(!bcrypt.compareSync(password,user.password!)){
+    return res.status(400).json({
+        message: 'Correo o contrasena no valido - PASSWORD'
+   })
+
+  }
+  const {role,name} = user
+
+  return res.status(200).json({
+     token:'',
+     user:{
+        name,
+        role,
+        email
+     }
+  })
+}
